a0814d07005fb0238defb0ac30d0d2c72aed7e7a
Anmol Singh <theanmolvirk.11@gmail.com>
2025-06-16 15:50:55 +0200

    token finding

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require('../models/user.model')
 
 const authMiddleware = async (req, res, next) => {
+  console.log(req)
   const token = req.cookies.token 
   if (!token) {
     return res.status(401).json({ error: "Access denied. No token provided." });
